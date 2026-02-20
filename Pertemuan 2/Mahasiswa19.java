b1e6dbdfa1ee71a916912033d85f34ef3f263d32
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2026-02-20 14:53:06 +0700

    Pertemuan 2 - Percobaan 1

---
+++ Pertemuan 2/Mahasiswa19.java
@@ -0,0 +1,33 @@
+public class Mahasiswa19 {
+    String nama, nim, kelas;
+    double ipk;
+
+    void tampilkanInformasi () {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas (String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void updateIpk (double ipkBaru){
+        ipk = ipkBaru;
+        if (ipk < 0.0 || ipk > 4.00) {
+            System.out.println("IPK tidak valid, harus antara 0.0 dan 4.00 ");
+        }
+    }
+
+    String nilaiKinerja(){
+        if (ipk >= 3.5){
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0){
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0){
+            return "Kinerja Cukup";
+        } else
+            return "Kinerja Kurang";
+    }
+}
