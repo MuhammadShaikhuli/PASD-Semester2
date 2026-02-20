dada7e5f3746cef929def8e4be2987f21f11ee8e
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2026-02-20 17:17:30 +0700

    Pertemuan 2 -Percobaan 3 - 1

--- Pertemuan 2/Mahasiswa19.java
+++ Pertemuan 2/Mahasiswa19.java
@@ -2,6 +2,16 @@ public class Mahasiswa19 {
     String nama, nim, kelas;
     double ipk;
 
+    public Mahasiswa19() {
+    }
+
+    public Mahasiswa19(String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+
     void tampilkanInformasi () {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
