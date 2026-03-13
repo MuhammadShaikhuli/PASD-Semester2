2d98546db1a3fabb4a5e8b2eb414898e8af70f1b
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2026-03-13 14:03:04 +0700

    Pertemuan 5 - Praktikum 3

---
+++ Minggu5/BruteForceDivideConquer/SumMain19.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumMain19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen = input.nextInt();
+
+        Sum19 sm = new Sum19(elemen);
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan keuntungan ke-" + (i + 1) + ": ");
+            sm.keuntungan[i] = input.nextDouble();
+        }
+
+        System.out.println("Total keuntungan menggunakan Brute Force: " + sm.totalBF());
+        System.out.println("Total keuntungan menggunakan Divide Conquer: " + sm.totalDC(sm.keuntungan, 0, elemen - 1));
+
+    }
+}
