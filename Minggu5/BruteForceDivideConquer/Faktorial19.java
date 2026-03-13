e25c75937677bfe47425b18ab4aec97f83f16e89
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2026-03-13 09:13:15 +0700

    Pertemuan 5 - Praktikum 1 - Part 1

---
+++ Minggu5/BruteForceDivideConquer/Faktorial19.java
@@ -0,0 +1,21 @@
+public class Faktorial19 {
+    int faktorialBF_While(int n) {
+    int faktor = 1;
+    int i = 1;
+    while (i <= n) {
+        faktor = faktor * i;
+        i++;
+    }
+    return faktor;
+}
+    
+    int faktorialDC(int n){
+        if(n==1){
+            return 1;
+        } else {
+            int faktor = n * faktorialDC(n-1);
+            return faktor;
+        }
+    }
+
+}
